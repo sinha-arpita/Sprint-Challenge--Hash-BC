24c2c56de50af69cbc716d6a9fbb7980b6fbee3c
Arpita Sinha <sinha.arpita@gmail.com>
2019-10-13 19:26:53 -0700

    MVP

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -14,10 +14,26 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
+
+    #
+    #Key : source
+    #Value : Ticket to be used in that source
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket)
+
+    #start from None, and reach till last detination
+    src = "NONE"
     route = [None] * length
+    i = 0;
+    while True:
+        ticket = hash_table_retrieve(hashtable, src)
+        route[i] = ticket.destination
+        i += 1
 
-    """
-    YOUR CODE HERE
-    """
+        if ticket.destination == "NONE":
+            break
 
+        src = ticket.destination
+
+    print (route)
     return route
